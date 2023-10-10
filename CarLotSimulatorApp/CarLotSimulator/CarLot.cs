4816c18790dae32af4829d1a2a5840ee5503d800
John Bright <jmbright400@gmail.com>
2023-10-10 10:04:42 -0500

    created a dealership.'

---
+++ CarLotSimulatorApp/CarLotSimulator/CarLot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class CarLot
+    {
+        public List<Car> DealerShip { get; set; } = new List<Car>();
+
+        public void CheckCars()
+        {
+            foreach (Car car in DealerShip)
+            {
+                Console.WriteLine($"This is a {car.Make} {car.Model}, manufactured in {car.Year}.");
+                car.MakeEngineNoise(car.EngineNoise);
+                car.MakeHonkNoise(car.HonkNoise);
+            }
+        }
+
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -6,28 +6,39 @@ namespace CarLotSimulator
     {
         static void Main(string[] args)
         {
-            //TODO
-
-            //Create a seperate class file called Car
-            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
-            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
-            //The methods should take one string parameter: the respective noise property
+            var carLot = new CarLot();
 
 
-            //Now that the Car class is created we can instanciate 3 new cars
-            //Set the properties for each of the cars
-            //Call each of the methods for each car
 
-            //*************BONUS*************//
+            var ford = new Car();
+            ford.Make = "Ford";
+            ford.Model = "Focus";
+            ford.Year = 2017;
+            ford.EngineNoise = "vroom";
+            ford.HonkNoise = "honk";
+            ford.IsDriveable = true;
 
-            // Set the properties utilizing the 3 different ways we learned about, one way for each car
+           carLot.DealerShip.Add(ford);
 
-            //*************BONUS X 2*************//
+            var chevy = new Car()
+            {
+                Make = "Chevy",
+                Model = "Task Force",
+                Year = 1958,
+                EngineNoise = "grinding",
+                HonkNoise = "beep",
+                IsDriveable = false
+            };
 
-            //Create a CarLot class
-            //It should have at least one property: a List of cars
-            //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
-            //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+           carLot.DealerShip.Add(chevy);
+
+            var lotus = new Car(2021, "Lotus", "Evora GT", "roar", "honk honk", true);
+
+           carLot.DealerShip.Add(lotus);
+   
+
+
+            carLot.CheckCars();
         }
     }
 }
